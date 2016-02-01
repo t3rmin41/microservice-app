c4c0f0efe59775e65105b73f21c1149d0e791b92
t3rmin41 <t33rmin41@gmail.com>
2016-02-01 17:07:21 +0200

    Remove logging in SingletonUUID

--- simple-microservice/src/main/java/com/simple/service/app/MyApp.java
+++ simple-microservice/src/main/java/com/simple/service/app/MyApp.java
@@ -4,9 +4,6 @@ import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import com.simple.service.config.MyAppConfig;
@@ -20,13 +17,7 @@ public class MyApp {
     
     public static void main(String[] args) {
         MDC.put("serviceInstanceId", singletonUUID.getUUID().toString()); // set UUID for log directory
-        ApplicationContext context = SpringApplication.run(MyApp.class, args);
+        SpringApplication.run(MyApp.class, args);
     }
 
-    @Bean
-    public EurekaInstanceConfigBean eurekaInstanceConfigBean() {
-        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean();
-        config.getMetadataMap().put("instanceId", singletonUUID.getUUID().toString()); // set UUID to observe in Eureka
-        return config;
-    }
 }
