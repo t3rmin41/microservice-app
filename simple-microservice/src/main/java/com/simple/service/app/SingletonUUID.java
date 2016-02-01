43951169f21773cc03f8df90c781a6c57bd94ad4
t3rmin41 <t33rmin41@gmail.com>
2016-02-01 17:11:22 +0200

    SingletonUUID logging removed

--- simple-microservice/src/main/java/com/simple/service/app/SingletonUUID.java
+++ simple-microservice/src/main/java/com/simple/service/app/SingletonUUID.java
@@ -2,18 +2,12 @@ package com.simple.service.app;
 
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class SingletonUUID {
-
-    private static Logger log = LoggerFactory.getLogger(SingletonUUID.class);
     private static SingletonUUID instance = new SingletonUUID();
 
     private UUID uuid = UUID.randomUUID();
 
     private SingletonUUID() {
-        log.info("SingletonUUID : Initializing instance with UUID : " + uuid.toString());
     }
     
     public static SingletonUUID getInstance() {
