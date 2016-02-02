cb3b68fd315c1e705ed604b6f22163d5aeb0a8b5
t3rmin41 <t33rmin41@gmail.com>
2016-02-02 14:14:44 +0200

    Logging application start

--- simple-microservice/src/main/java/com/simple/service/app/MyApp.java
+++ simple-microservice/src/main/java/com/simple/service/app/MyApp.java
@@ -1,6 +1,7 @@
 package com.simple.service.app;
 
-import org.slf4j.MDC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -16,10 +17,12 @@ import com.simple.service.config.MyAppConfig;
 @Import({MyAppConfig.class})
 public class MyApp {
 
+    private static Logger log = LoggerFactory.getLogger(MyApp.class);
+    
     private static SingletonUUID singletonUUID = SingletonUUID.getInstance();
     
     public static void main(String[] args) {
-        //MDC.put("serviceInstanceId", singletonUUID.getUUID().toString()); // set UUID for log directory
+        log.info("Starting service with UUID " + singletonUUID.getUUID().toString());
         ApplicationContext context = SpringApplication.run(MyApp.class, args);
     }
 
