91b973f8fb1eaea87d7411549dafb14656a39c16
t3rmin41 <t33rmin41@gmail.com>
2016-02-02 14:16:26 +0200

    Logging application start in main()

--- simple-microservice/src/main/java/com/simple/service/app/MyApp.java
+++ simple-microservice/src/main/java/com/simple/service/app/MyApp.java
@@ -1,5 +1,7 @@
 package com.simple.service.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,9 +18,12 @@ import com.simple.service.config.MyAppConfig;
 @Import({MyAppConfig.class})
 public class MyApp {
 
+    private static Logger log = LoggerFactory.getLogger(MyApp.class);
+    
     private static SingletonUUID singletonUUID = SingletonUUID.getInstance();
     
     public static void main(String[] args) {
+        log.warn("Starting service with UUID " + singletonUUID.getUUID().toString());
         ApplicationContext context = SpringApplication.run(MyApp.class, args);
     }
 
