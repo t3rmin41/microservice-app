966800a32fc6ed7305942eec38dd5dffb53e72b9
t3rmin41 <t33rmin41@gmail.com>
2016-02-04 13:42:56 +0200

    Added Hystrix dashboard to project

---
+++ hystrix-dashboard/src/main/java/dk/csc/kombit/ee/monitoring/hystrix/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,33 @@
+package dk.csc.kombit.ee.monitoring.hystrix.dashboard;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
+@EnableHystrixDashboard
+public class HystrixDashboardApplication extends SpringBootServletInitializer {
+    
+    @RequestMapping("/")
+    public String home() {
+        return "forward:/hystrix";
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HystrixDashboardApplication.class).web(true);
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
+    }
+
+}
